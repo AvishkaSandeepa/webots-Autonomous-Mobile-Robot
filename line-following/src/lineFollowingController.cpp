489e657faf19ae38a5f9ad95d3efd7bc56f304fc
AvishkaSandeepa <cavishkasandeepa98@gmail.com>
2021-05-30 16:37:02 +0530

    Add several function for the PD controlling mechanism and for motor driving mechanism

--- line-following/src/lineFollowingController.cpp
+++ line-following/src/lineFollowingController.cpp
@@ -1,77 +1,163 @@
 //Author : Avishka Sandeepa
-//Modified date : 30/5/2021
+//Modified date : 24/5/2021
 
 // Added include files
 #include <webots/Robot.hpp>
 #include <webots/Motor.hpp>
 #include <webots/DistanceSensor.hpp>
+#include <webots/PositionSensor.hpp>
+
 
 #define TIME_STEP 64
 #define MAX_SPEED 6.28
 
+double baseSpeed = 3.14;
+double le = 0;
+double set = 3500;
 double sensorValues[8];
+
 
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
 
 
-//---------------------main function----------------------------
+//----function to read the values of the sensors and convert to binary--------------------------
+void read(){
+ for (int i = 0; i < 8; i++){
+   if (sensorValues[i] > 5.5){
+     sensorValues[i] = 1;
+   }else{
+     sensorValues[i] = 0;
+   }
+ }
+}
+//----------------------end of the read() function---------------------------------------------------
+
+
+//------function for PD calculation-------------------------------
+double PID_calc(){
+ double average = 0;
+ double sum = 0;
+ for (int i = 0; i < 8 ; i++){ 
+   average += sensorValues[i] * i * 1000;
+   sum += sensorValues[i];
+ }
+ 
+ double position = average / sum;  //---------weighted mean-----------------------------------
+ 
+ double kp = 0.001;
+ double kd = 0.001;
+ //double ki = 0.0;
+ double e = position - set;
+ double p = kp * e;
+ double d = kd * (e - le);
+ //double i = ki * (e + le);
+ double offset = p + d;
+ le = e;
+ return offset;
+}
+//--------------end of the PID_calc() function----------------------------------------
+
+
+//---------------------function for motor driving------------------------------
+double Mdriver(double speed){
+
+ if (speed > 0){
+   if (speed > 6.28){
+     speed = 6.28;
+   }
+ }else{
+   if (speed < -6.28){
+     speed = -6.28;
+   }
+ }
+ 
+ return speed;
+}
+//---------------------end of the driver() function----------------------------
+
 
 int main(int argc, char **argv) {
-    Robot *robot = new Robot();
+ Robot *robot = new Robot();
  
-    // get a handler to the motors and set target position to infinity (speed control)
-    Motor *leftMotor = robot->getMotor("left wheel motor");
-    Motor *rightMotor = robot->getMotor("right wheel motor");
-    leftMotor->setPosition(INFINITY);
-    rightMotor->setPosition(INFINITY);
-
-    //initialize the sensors for line detection-------------
-
-    // initialize sensors
-    DistanceSensor *ir[8];
-    char sensorNames[8][4] = {
-    "ir0", "ir1", "ir2", "ir3",
-    "ir4", "ir5", "ir6", "ir7"
-    };
-    
-
-
-    //enable the sensors to get measurements
-    for (int i = 0; i < 8; i++) {
-        ir[i] = robot->getDistanceSensor(sensorNames[i]);
-        ir[i]->enable(TIME_STEP);
-    }
+ // get a handler to the motors and set target position to infinity (speed control)
+ Motor *leftMotor = robot->getMotor("left wheel motor");
+ Motor *rightMotor = robot->getMotor("right wheel motor");
+ leftMotor->setPosition(INFINITY);
+ rightMotor->setPosition(INFINITY);
  
+
+
+ // initialize sensors
+ DistanceSensor *ir[8];
+ char sensorNames[8][4] = {
+ "ir0", "ir1", "ir2", "ir3",
+ "ir4", "ir5", "ir6", "ir7"
+ };
+ 
+
+
+//enable the sensors to get measurements
+for (int i = 0; i < 8; i++) {
+  ir[i] = robot->getDistanceSensor(sensorNames[i]);
+  ir[i]->enable(TIME_STEP);
+}
+
+
+
 //---------------------main loop---------------------------------------------
-    while (robot->step(TIME_STEP) != -1){
-
-    //------------get the values from the sensors------------------
-        for (int i = 0; i < 8 ; i++){
-            sensorValues[i] = ir[i]->getValue();
-        }
-    //setup motor speeds
-        leftMotor->setVelocity(MAX_SPEED);
-        rightMotor->setVelocity(MAX_SPEED);
-
-    //-----print the values----------------
-        std::cout<<"ir0 = "<<sensorValues[0]<<"  ";
-        std::cout<<"ir1 = "<<sensorValues[1]<<"  ";
-        std::cout<<"ir2 = "<<sensorValues[2]<<"  ";
-        std::cout<<"ir3 = "<<sensorValues[3]<<"  ";
-        std::cout<<"ir4 = "<<sensorValues[4]<<"  ";
-        std::cout<<"ir5 = "<<sensorValues[5]<<"  ";
-        std::cout<<"ir6 = "<<sensorValues[6]<<"  ";
-        std::cout<<"ir7 = "<<sensorValues[7]<<std::endl;
+ while (robot->step(TIME_STEP) != -1){
 
 
-    
+ // read sensors outputs
+ 
+ for (int i = 0; i < 8 ; i++){
+ sensorValues[i] = ir[i]->getValue();
+ }
+ 
 
-    }
+ read();
+ 
+ 
+   double offset = PID_calc();
+   
+   //------------------------------------------------------------
+  
+   double left = baseSpeed + offset;
+   double right = baseSpeed - offset;
+   
+   //-------------------------------------------------------------
+   
+   double leftSpeed = Mdriver(left);
+   double rightSpeed = Mdriver(right);
+   
+   //---------------------------------------
+   
+   leftMotor->setVelocity(leftSpeed);
+   rightMotor->setVelocity(rightSpeed);
+   
+   std::cout<<"ir0 = "<<sensorValues[0]<<"  ";
+   std::cout<<"ir1 = "<<sensorValues[1]<<"  ";
+   std::cout<<"ir2 = "<<sensorValues[2]<<"  ";
+   std::cout<<"ir3 = "<<sensorValues[3]<<"  ";
+   std::cout<<"ir4 = "<<sensorValues[4]<<"  ";
+   std::cout<<"ir5 = "<<sensorValues[5]<<"  ";
+   std::cout<<"ir6 = "<<sensorValues[6]<<"  ";
+   std::cout<<"ir7 = "<<sensorValues[7]<<std::endl;
+     
+   std::cout<<" offset : "<<offset<<std::endl;
+   
+ 
+  
+  
+//----------call the pre defined functions------------------------
+ 
 
-    delete robot;
+ }
 
-    return 0;
+ delete robot;
+
+ return 0;
 }
 
 
