90f55ff17ff0c7b14cd8b1c589a9aa1c33f08914
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2021-07-09 00:44:23 +0530

    Created test robot inside provided test arena

---
+++ EN2532_Arena_v4_1/controllers/test/test.cpp
@@ -0,0 +1,37 @@
+//Author : Avishka Sandeepa
+//Modified date : 30/5/2021
+
+// Added include files
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+
+#define TIME_STEP 64
+#define MAX_SPEED 6.28
+
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+
+
+//---------------------main function----------------------------
+
+int main(int argc, char **argv) {
+ Robot *robot = new Robot();
+ 
+ // get a handler to the motors and set target position to infinity (speed control)
+ Motor *leftMotor = robot->getMotor("left motor");
+ Motor *rightMotor = robot->getMotor("right motor");
+ leftMotor->setPosition(INFINITY);
+ rightMotor->setPosition(INFINITY);
+ 
+//---------------------main loop---------------------------------------------
+ while (robot->step(TIME_STEP) != -1){
+
+//setup motor speeds
+    leftMotor->setVelocity(MAX_SPEED -3);
+    rightMotor->setVelocity(MAX_SPEED);
+
+ }
+
+ delete robot;
+ return 0;
+}
