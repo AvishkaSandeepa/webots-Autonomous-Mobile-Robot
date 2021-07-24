71bdfaa22628e9b7f843742453365e8fd3b921b5
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2021-07-24 19:26:59 +0530

    Maze area case 1 completed and debugged.

--- Wall_Line_Stable/controllers/gate_controller1/gate_controller1.cpp
+++ Wall_Line_Stable/controllers/gate_controller1/gate_controller1.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
       else time = -1;
       
       // double PSVal = gate_ps->getValue();
-      cout << "time = " << time << '\n';
+      //cout << "time = " << time << '\n';
       time++;
 
        // cout << "Position = " << PSVal << '\n';
