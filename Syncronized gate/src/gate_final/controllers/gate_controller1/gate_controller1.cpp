3a5f85de723484a45ed341853e2240aef410d394
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2021-07-24 16:31:51 +0530

    gate controller completed

--- Syncronized gate/src/gate_final/controllers/gate_controller1/gate_controller1.cpp
+++ Syncronized gate/src/gate_final/controllers/gate_controller1/gate_controller1.cpp
@@ -3,9 +3,8 @@
 #include <webots/Motor.hpp>
 #include <webots/PositionSensor.hpp>
 
-#define TIME_STEP 64
+#define TIME_STEP 1000
 
-// All the webots classes are defined in the "webots" namespace
 using namespace webots;
 using namespace std;
 
@@ -15,19 +14,25 @@ int main(int argc, char **argv) {
     Motor *gateMotor  = robot->getMotor("gateMotor");
     gateMotor->setPosition(INFINITY);
     gateMotor->setVelocity(0);
-    PositionSensor *gate_ps = robot->getPositionSensor("gate_ps");
     
+    PositionSensor *gate_ps = robot->getPositionSensor("gate_ps");   
     gate_ps->enable(TIME_STEP);
     
-    while (robot->step(TIME_STEP) != -1) {
+    int time = 0;
+
+    while (robot->step(TIME_STEP) != -1) {      
+
+      if(time < 1) gateMotor->setVelocity(-1.5);
+      else if(  1 <= time && time <= 9 ) gateMotor->setVelocity(0);
+      else if(time < 11) gateMotor->setVelocity(1.5);
+      else if(11 <= time && time < 20) gateMotor->setVelocity(0);
+      else time = -1;
       
-      double PSVal = gate_ps->getValue();
-      gateMotor->setVelocity(-2);
-      
-      
-      
-      
-      cout << "Position = " << PSVal << '\n';
+      // double PSVal = gate_ps->getValue();
+      cout << "time = " << time << '\n';
+      time++;
+
+       // cout << "Position = " << PSVal << '\n';
       
   };
 
