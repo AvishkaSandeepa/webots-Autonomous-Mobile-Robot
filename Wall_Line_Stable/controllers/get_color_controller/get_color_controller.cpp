f08926e2f301aeb4a6c783dd2e5beecfab6933ca
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2021-07-28 14:53:25 +0530

    Updated and finalized get_color controller.

--- Wall_Line_Stable/controllers/get_color_controller/get_color_controller.cpp
+++ Wall_Line_Stable/controllers/get_color_controller/get_color_controller.cpp
@@ -10,31 +10,36 @@ using namespace std;
 
 string colors[4] = {"gray","red","green","blue"};
 int color;
+Camera *camera;
+void get_color_at(int x, int y);
+
 
 
 int main(int argc, char **argv) {
 
   Robot *robot = new Robot();
-  
-  Camera *camera;
   camera=robot->getCamera("color_sensor");
   camera->enable(TIME_STEP);
-  void get_color();
-  
-  
   
   while (robot->step(TIME_STEP) != -1) {
   
+  get_color_at(32,20);
+  get_color_at(32,50);
+  
+  
+  }
+  delete robot;
+  return 0;  // EXIT_SUCCESS
+}
+
+void get_color_at(int x, int y){
+  
   const unsigned char* image = camera->getImage();
   int image_width = camera->getWidth();
-  int x = 32; int y = 32; // point of color extraction
+   
   int r = camera->imageGetRed(image, image_width, x, y);
   int g = camera->imageGetGreen(image, image_width, x, y);
   int b = camera->imageGetBlue(image, image_width, x, y);
-  // cout << "red = "<< r << "\tGreen = " << g  << "\tBlue = " << b << '\n';
-  
-  // color detection
-  //  red 1 green 2 blue 3
   
   if(r > g && r > b) color = 1;
   if(g > r && g > b) color = 2;
@@ -42,17 +47,4 @@ int main(int argc, char **argv) {
   
   cout << "Detected color = " << colors[color] << '\n';
   
-  }
-  delete robot;
-  return 0;  // EXIT_SUCCESS
 }
-
-// void get_color(){
-  // const unsigned char* image = camera->getImage();
-  // int image_width = camera->getWidth();
-  // int x = 32; int y = 32; 
-  // int r = camera->imageGetRed(image, image_width, x, y);
-  // int g = camera->imageGetGreen(image, image_width, x, y);
-  // int b = camera->imageGetBlue(image, image_width, x, y);
-  // cout << "red = "<< r << "\tGreen = " << g  << "\tBlue = " << b << '\n';
-// }
