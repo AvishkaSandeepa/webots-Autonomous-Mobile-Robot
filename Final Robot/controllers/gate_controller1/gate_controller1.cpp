1f5988fd5a9ea8ef102aff2d2be3030bec029ede
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2021-07-29 19:30:43 +0530

    Formatted gate controllers

--- Final Robot/controllers/gate_controller1/gate_controller1.cpp
+++ Final Robot/controllers/gate_controller1/gate_controller1.cpp
@@ -8,36 +8,33 @@
 using namespace webots;
 using namespace std;
 
-int main(int argc, char **argv) {
+int main (int argc, char** argv) {
     // create the Robot instance.
-    Robot *robot = new Robot();
-    Motor *gateMotor  = robot->getMotor("gateMotor");
-    gateMotor->setPosition(INFINITY);
-    gateMotor->setVelocity(0);
-    
-    PositionSensor *gate_ps = robot->getPositionSensor("gate_ps");   
-    gate_ps->enable(TIME_STEP);
-    
+    Robot* robot = new Robot ();
+    Motor* gateMotor = robot->getMotor ("gateMotor");
+    gateMotor->setPosition (INFINITY);
+    gateMotor->setVelocity (0);
+
+    PositionSensor* gate_ps = robot->getPositionSensor ("gate_ps");
+    gate_ps->enable (TIME_STEP);
+
     int time = 0;
 
-    while (robot->step(TIME_STEP) != -1) {      
-      
-      if(time < 1) gateMotor->setVelocity(-1.5);
-      else if(  1 <= time && time <= 9 ) gateMotor->setVelocity(0);
-      else if(time < 11) gateMotor->setVelocity(1.5);
-      else if(11 <= time && time < 20) gateMotor->setVelocity(0);
-      else time = -1;
-      
-      // double PSVal = gate_ps->getValue();
-      //cout << "time = " << time << '\n';
-      time++;
+    while ( robot->step (TIME_STEP) != -1 ) {
 
-       // cout << "Position = " << PSVal << '\n';
-      
-  };
+        if ( time < 1 ) gateMotor->setVelocity (-1.5);
+        else if ( 1 <= time && time <= 9 ) gateMotor->setVelocity (0);
+        else if ( time < 11 ) gateMotor->setVelocity (1.5);
+        else if ( 11 <= time && time < 20 ) gateMotor->setVelocity (0);
+        else time = -1;
 
-  // Enter here exit cleanup code.
+        time++;
 
-  delete robot;
-  return 0;
+
+    };
+
+    // Enter here exit cleanup code.
+
+    delete robot;
+    return 0;
 }
--- Final Robot/controllers/gate_controller2/gate_controller2.cpp
+++ Final Robot/controllers/gate_controller2/gate_controller2.cpp
@@ -8,37 +8,33 @@
 using namespace webots;
 using namespace std;
 
-int main(int argc, char **argv) {
+int main (int argc, char** argv) {
     // create the Robot instance.
-    Robot *robot = new Robot();
-    Motor *gateMotor  = robot->getMotor("gateMotor");
-    gateMotor->setPosition(INFINITY);
-    gateMotor->setVelocity(0);
-    
-    PositionSensor *gate_ps = robot->getPositionSensor("gate_ps");   
-    gate_ps->enable(TIME_STEP);
-    
+    Robot* robot = new Robot ();
+    Motor* gateMotor = robot->getMotor ("gateMotor");
+    gateMotor->setPosition (INFINITY);
+    gateMotor->setVelocity (0);
+
+    PositionSensor* gate_ps = robot->getPositionSensor ("gate_ps");
+    gate_ps->enable (TIME_STEP);
+
     int time = 0;
 
-    while (robot->step(TIME_STEP) != -1) {      
-      
-      if(  0 <= time && time < 3 ) gateMotor->setVelocity(0);
-      else if(time < 4) gateMotor->setVelocity(-1.5);
-      else if(  4 <= time && time < 12 ) gateMotor->setVelocity(0);
-      else if(time < 13) gateMotor->setVelocity(1.5);
-      else if(13 <= time && time < 20) gateMotor->setVelocity(0);
-      else time = -1;
-      
-      // double PSVal = gate_ps->getValue();
-      //cout << "time = " << time << '\n';
-      time++;
+    while ( robot->step (TIME_STEP) != -1 ) {
 
-       // cout << "Position = " << PSVal << '\n';
-      
-  };
+        if ( 0 <= time && time < 3 ) gateMotor->setVelocity (0);
+        else if ( time < 4 ) gateMotor->setVelocity (-1.5);
+        else if ( 4 <= time && time < 12 ) gateMotor->setVelocity (0);
+        else if ( time < 13 ) gateMotor->setVelocity (1.5);
+        else if ( 13 <= time && time < 20 ) gateMotor->setVelocity (0);
+        else time = -1;
 
-  // Enter here exit cleanup code.
+        time++;
 
-  delete robot;
-  return 0;
+
+    };
+
+
+    delete robot;
+    return 0;
 }
